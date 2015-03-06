f7eb281e69cff1a78b379e78ee947acfd9a58063
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-03-06 11:47:29 +0100

    Fixed compilation error

---
+++ omod/src/main/java/org/openmrs/module/dataexchange/web/controller/MetadataSharingParser.java
@@ -0,0 +1,13 @@
+package org.openmrs.module.dataexchange.web.controller;
+
+import java.io.InputStream;
+import java.util.Set;
+
+
+public class MetadataSharingParser {
+
+	public Set<Integer> parseConceptIds(InputStream inputStream) {
+	    throw new RuntimeException("Not supported");
+    }
+	
+}

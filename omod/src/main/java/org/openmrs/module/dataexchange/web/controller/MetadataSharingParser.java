469535f8a33b09e432e0eca24d78774bb09b0172
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-03-06 12:58:10 +0100

    Fixed issue with MetadataSharingParser

--- omod/src/main/java/org/openmrs/module/dataexchange/web/controller/MetadataSharingParser.java
+++ omod/src/main/java/org/openmrs/module/dataexchange/web/controller/MetadataSharingParser.java
@@ -3,7 +3,9 @@ package org.openmrs.module.dataexchange.web.controller;
 import java.io.InputStream;
 import java.util.Set;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class MetadataSharingParser {
 
 	public Set<Integer> parseConceptIds(InputStream inputStream) {
